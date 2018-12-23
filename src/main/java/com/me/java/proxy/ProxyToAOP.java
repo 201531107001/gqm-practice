faf6cfde9491a39ea296b9cd7540871c4e02cba2
gqm <2571197208@qq.com>
2018-12-23 23:03:28 +0800

    java的动态代理

---
+++ src/main/java/com/me/java/proxy/ProxyToAOP.java
@@ -0,0 +1,70 @@
+package com.me.java.proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+interface IVehical {
+
+    void run(int speed);
+
+}
+
+//concrete implementation
+class Car implements IVehical {
+    public void run(int speed) {
+        System.out.println("Car is running,speed:"+speed);
+    }
+
+}
+
+//proxy class
+class VehicalProxy {
+
+    private IVehical vehical;
+
+    public VehicalProxy(IVehical vehical) {
+        this.vehical = vehical;
+    }
+
+    public IVehical create() {
+        final Class<?>[] interfaces = new Class[] { IVehical.class };
+        final VehicalInvacationHandler handler = 
+                new VehicalInvacationHandler(vehical);
+
+        return (IVehical) Proxy.newProxyInstance
+                (IVehical.class.getClassLoader(), interfaces, handler);
+    }
+
+    public class VehicalInvacationHandler implements InvocationHandler {
+
+        private final IVehical vehical;
+
+        public VehicalInvacationHandler(IVehical vehical) {
+            this.vehical = vehical;
+        }
+
+        public Object invoke(Object proxy, Method method, Object[] args)
+                throws Throwable {
+            System.out.println("--before running...");
+            Object ret = method.invoke(vehical, args);
+            System.out.println("--after running...");
+            return ret;
+        }
+    }
+}
+
+public class ProxyToAOP {
+    public static void main(String[] args) {
+
+        IVehical car = new Car();
+        VehicalProxy proxy = new VehicalProxy(car);
+
+        // 这个对象将会代理car
+        IVehical proxyObj = proxy.create();
+        proxyObj.run(50);
+    }
+}
+/*
+ * output: --before running... Car is running --after running...
+ */
---
+++ src/main/java/com/me/java/proxy/ProxyToConfigFactory.java
@@ -0,0 +1,113 @@
+package com.me.java.proxy;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Properties;
+
+@Target({ ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@interface Value {
+
+    /**
+     * The actual value expression: e.g. "#{systemProperties.myProp}".
+     */
+    String value();
+}
+
+/**
+ * config interfaces, map the config properties file: db.url = db.validation =
+ * true db.pool.size = 100
+ */
+interface IConfig {
+
+    @Value("db.url")
+    String dbUrl();
+
+    @Value("db.validation")
+    boolean isValidated();
+
+    @Value("db.pool.size")
+    int poolSize();
+
+}
+
+//proxy class
+public final class ProxyToConfigFactory {
+
+    private ProxyToConfigFactory() {
+    }
+
+    public static IConfig create(final InputStream is) 
+            throws IOException {
+
+        final Properties properties = new Properties();
+        properties.load(is);
+
+        return (IConfig) Proxy.newProxyInstance
+                (IConfig.class.getClassLoader(), new Class[] { IConfig.class },
+                new PropertyMapper(properties));
+
+    }
+
+    public static final class PropertyMapper implements InvocationHandler {
+
+        private final Properties properties;
+
+        public PropertyMapper(Properties properties) {
+            this.properties = properties;
+        }
+
+        public Object invoke(Object proxy, Method method, 
+                Object[] args) throws Throwable {
+
+            final Value value = method.getAnnotation(Value.class);
+
+            if (value == null)
+                return null;
+
+            String property = properties.getProperty(value.value());
+            if (property == null)
+                return (null);
+
+            final Class<?> returns = method.getReturnType();
+            if (returns.isPrimitive()) {
+                if (returns.equals(int.class))
+                    return (Integer.valueOf(property));
+                else if (returns.equals(long.class))
+                    return (Long.valueOf(property));
+                else if (returns.equals(double.class))
+                    return (Double.valueOf(property));
+                else if (returns.equals(float.class))
+                    return (Float.valueOf(property));
+                else if (returns.equals(boolean.class))
+                    return (Boolean.valueOf(property));
+            }
+
+            return property;
+        }
+
+    }
+
+    public static void main(String[] args) 
+            throws FileNotFoundException, IOException {
+
+        IConfig config = ProxyToConfigFactory
+                        .create(new FileInputStream("doc/db.properties"));
+        String dbUrl = config.dbUrl();
+        boolean isLoginValidated = config.isValidated();
+        int dbPoolSize = config.poolSize();
+        System.out.println(dbUrl+" "+dbPoolSize+" "+isLoginValidated);
+    }
+
+}
